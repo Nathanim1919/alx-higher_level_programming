4de74b2b05adb295e55a8b9d734e19a75f8b0260
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-07 14:12:53 -0800

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for x in str:
+        if (ord(x) <= ord('Z') and ord(x) >= ord('A')):
+            print("{}".format(x), end='')
+        else:
+            if (ord(x) >= ord('a') and ord(x) <= ord('z')):
+                    print("{}".format(x.capitalize()), end='')
