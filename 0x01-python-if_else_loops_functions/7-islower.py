b81c6afaf607cc0478708ef448ce0d46c486d719
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-07 13:52:18 -0800

    is lower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for i in range(97, 123):
-        if c == chr(i):
-            return True
-    return False
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
