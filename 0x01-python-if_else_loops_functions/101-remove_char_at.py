7ee5693d55bef68ed9df745170cca42ef494818e
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-08 02:48:17 -0800

    Remove at position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    newstr = str
+    if (n == 0):
+        newstr = newstr[1:]
+    elif (n == (len(str) - 1))
+        newstr = newstr[:-1]
+    else:
+        newstr = newstr[:n]+""+newstr[n+1:]
+    return newstr
