83bb1e6f2c250f93d074ffc3a3ba13c57b0c1ec7
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-07 23:53:25 -0800

    FizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(101):
+        if (i == 100):
+            print("Buzz")
+        else:
+            if (i > 0 and i % 3 == 0 and i % 5 == 0):
+                print("FizzBuzz", end=", ")
+            elif (i > 0 and i % 3 == 0):
+                print("Fizz", end=", ")
+            elif (i > 0 and i % 5 == 0):
+                print("Buzz", end=", ")
+            else:
+                print(i, end=", ")
