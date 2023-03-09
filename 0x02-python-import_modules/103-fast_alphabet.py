300b5bdc8b42280f63010476c299fc774ea376c6
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-09 08:43:53 -0800

    fast alphabet

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from fast_alphabet import printA
+fast_alphabet.printA()
---
+++ 0x02-python-import_modules/fast_alphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def printA():
+    i = 26
+    while (i < 93):
+        print(chr(i), end="")
+        i = i + 1
