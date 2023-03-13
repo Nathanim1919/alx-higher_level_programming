49ab456282fb8a9e76018c58b22bf08c75262d04
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-13 00:14:01 -0700

    Tuple addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    first = if tuple_a[0] else 0 + if tuple_b[0] tuple_b[0] else 0
+    second = if tuple_a[1] tuble_a[1] else 0 + if tuple_b[1] tuple_b[1] else 0
+    new_tuple = (first, second)
+    return new_tuple
