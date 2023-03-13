12e6ff9b0664384b672daffc915fe710c3083ca0
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-13 00:46:28 -0700

    fnd th max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if (not my_list):
+    if not my_list:
         return None
-    else:
-        max = 0
-        for i in my_list:
-            if i > max:
-                max = i
-        return max
+    biggest = 0
+    for elem in my_list:
+        if elem > biggest:
+            biggest = elem
+    return biggest
