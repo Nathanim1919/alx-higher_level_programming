ddbcf3dcda494d91766c0a8ec36fd510ea3c70f7
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-15 09:11:25 -0700

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    square =[]
-    for row in matrix:
-        rows = []
-        for i in row:
-            rows.append(i**2)
-        square.append(rows)
-    return square
+    if not matrix:
+        print()
+    return [[item**2 for item in row] for row in matrix]
