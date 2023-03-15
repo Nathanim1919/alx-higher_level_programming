2544b12df90faedd5ce294c270494ab8bbc44907
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-15 08:51:22 -0700

    unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    result = 0
+    lists
+    for i in my_list:
+        if i not in lists:
+            result += i
+            lists.append(i)
+    return result
