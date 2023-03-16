01970499f97aa0fd6ae69121198ee93f2fcb0338
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-16 01:15:34 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    best = ''
     max = 0
     if len(a_dictionary.keys() == 0):
         return None
-    else:
-        for key, value in a_dictionary.items():
-            if a_dictionary[key] > max:
-                max = value
-        return max
+    for key, value in a_dictionary.items():
+        if a_dictionary[key] > max:
+            max = value
+            best = key
+    return best
