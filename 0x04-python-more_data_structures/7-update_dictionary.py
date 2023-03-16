15d20b629c1ede804a46c518ff9b274dd5ca16b6
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-16 00:45:21 -0700

    update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for k, v in a_dictionary.items():
-        if k == key:
-            a_dictionary[k] = value
-        else:
-            a_dictionary[key]  = value
+    a_dictionary[key]  = value
     return a_dictionary
