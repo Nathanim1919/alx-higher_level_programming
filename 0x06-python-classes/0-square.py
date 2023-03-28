d3bca07a44b0ae687c40b1862d86ab6ad983d65d
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-28 14:56:13 -0700

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 class Square:
+    """ A class that defines a square.
+
+    This class has no proprties or methods, but can be used as a template
+    for creating objects that represent squares.
+
+    Attributes:
+        None
+    """
+
     pass
