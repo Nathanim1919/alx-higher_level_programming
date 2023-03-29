5bfe12cc5f427550f376f49c59b9a9c17b646feb
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-29 01:23:00 -0700

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" a class called Square"""
+class Square:
+    """instation of size attributes"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
