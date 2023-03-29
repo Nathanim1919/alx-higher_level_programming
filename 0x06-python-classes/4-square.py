5a63e1007894a9eb73e66417876439bf9f4db332
Nathanim1919 <nathanim2tadele@gmail.com>
2023-03-29 01:56:34 -0700

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" a class called Square"""
+
+class Square:
+    """represent a square"""
+
+    def __init__(self, size=0):
+        """ Instiation a new Square.
+        Args:
+            size (int): The size of the new square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        slef.__size = value
+
+    def area(self):
+        """public instance method called area"""
+        return self.__size ** 2
