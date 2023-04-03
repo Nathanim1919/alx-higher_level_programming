942e06adcbb096ea27febaf528b6159f61f0c18a
Nathanim1919 <nathanim2tadele@gmail.com>
2023-04-03 03:32:01 -0700

    Real difinition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif height < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
