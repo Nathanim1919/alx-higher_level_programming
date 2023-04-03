8e1222769ebd2bc91fc04ea8e5be9e2ba0b8859c
Nathanim1919 <nathanim2tadele@gmail.com>
2023-04-03 04:47:05 -0700

    Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -53,3 +53,6 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width] * self.__height)
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
