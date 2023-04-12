895980aa7dbe2b3fb69199d71bdaf116ae7117be
Nathanim1919 <nathanim2tadele@gmail.com>
2023-04-12 12:52:53 -0700

    Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" module that contains one function """
+def is_same_class(obj, a_class):
+    """ function definition
+        Arg:
+            -obj
+            -class
+        return:bool
+    """
+    return isinstance(obj, class)
