8276caca2e1540512b07bb4c4a1c1d9807fea852
Nathanim1919 <nathanim2tadele@gmail.com>
2023-04-12 16:05:43 -0700

    only subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """Module inherits_from 
-this module containes one function called inherits_from(obj, a_class)"""
+this module containes one function 
+called inherits_from(obj, a_class)"""
 
 def inherits_from(obj, a_class):
     """
-    -Checks if the object is an instance of a class that inherited from the specified class.
+    -Checks if the object is an instance of 
+    a class that inherited from the specified class.
 
     Args:
     -obj: an object
     -a_class: a class or type
 
     Returns:
-    -True if an object is an instance of a class that inhirited from the specified class.
+    -True if an object is an instance of a class
+    that inhirited from the specified class.
     """
 
 
