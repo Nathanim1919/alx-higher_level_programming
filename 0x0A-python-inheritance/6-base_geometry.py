550728dde8be4050909fefc0d45314ec2c320cf5
Nathanim1919 <nathanim2tadele@gmail.com>
2023-04-12 14:09:54 -0700

    imporove geometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -2,7 +2,16 @@
 """ this module containe a class called BaseGeometry """
 
 class BaseGeometry:
-    """ a class """
+    """ 
+    -Base class for geometry objects.
+
+    """
 
     def area(self):
+        """
+        calculate the are of the geomtry object.
+
+        Raises:
+        - NotImplementedError: if the method is not implemented by the subclass.
+        """
         raise NotImplementedError("area() is not implemented")
