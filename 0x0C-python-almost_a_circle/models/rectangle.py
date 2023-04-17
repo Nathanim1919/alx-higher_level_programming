c482e2ceb16a23728636ac4bb742977e3a0adf1f
Nathanim1919 <nathanim2tadele@gmail.com>
2023-04-17 15:41:21 -0700

    First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,7 @@ class Rectangle(Base):
     """Class that defines a Rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        super.__init__(id)
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
