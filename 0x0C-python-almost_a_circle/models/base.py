8a60fd2179df3dd09b8a53d02a599e8dc597be5b
Nathanim1919 <nathanim2tadele@gmail.com>
2023-04-17 09:24:15 -0700

    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

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" this modlue icludes one base class"""
+
+
+class Base:
+    """
+    A base class to manage the id attribute in all future classes in this project.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = __nb_objects
+        else:
+            self.id = id
