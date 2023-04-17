18c1894a495bb0b2650dd080b79d1a30f873adb0
Nathanim1919 <nathanim2tadele@gmail.com>
2023-04-17 15:28:31 -0700

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,6 +12,6 @@ class Base:
     def __init__(self, id=None):
         if id is None:
             Base.__nb_objects += 1
-            self.id = __nb_objects
+            self.id = Base.__nb_objects
         else:
             self.id = id
