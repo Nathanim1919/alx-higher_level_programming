27d7efa642e802084c5dfaca955e8d47b5ad2c51
Nathanim1919 <nathanim2tadele@gmail.com>
2023-04-17 15:17:27 -0700

    first rectangle

--- 0x0C-python-almost_a_circle/1-main.py
+++ 0x0C-python-almost_a_circle/1-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""1- main"""
+""" 1-main """
 from models.rectangle import Rectangle
 
-if __name__ == "__main++":
+if __name__ == "__main__":
 
     r1 = Rectangle(10, 2)
     print(r1.id)
