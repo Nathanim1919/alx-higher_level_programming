1d5adc860f0cc121eeeb039bf0ede51474b91902
Nathanim1919 <nathanim2tadele@gmail.com>
2023-04-21 01:12:11 +0300

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,4 +10,5 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
     
     def __str__(self):
+        """Returns string representation of the object"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
