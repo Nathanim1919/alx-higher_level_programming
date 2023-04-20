4ed9c346313469f28e0a71ab9897ad8de8f32a17
Nathanim1919 <nathanim2tadele@gmail.com>
2023-04-21 00:06:52 +0300

    Update #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,3 +82,16 @@ class Rectangle(Base):
     def __str__(self) -> str:
         """ __str__ """
         return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.x, self.y, self.width, self.height)
+    
+    def update(self, *args):
+        """update the rectangle attributes"""
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
