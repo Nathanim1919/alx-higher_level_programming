fa9ff2efae3a9fc09257c4cc8fd3ec26c4c19ea5
Nathanim1919 <nathanim2tadele@gmail.com>
2023-04-21 00:38:34 +0300

    Square

---
+++ 0x0C-python-almost_a_circle/models/Square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this module inherits from Base class the Base module"""
+
+from models.base import Base
+
+class Square(Base):
+    """Class that defines a Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+    
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
