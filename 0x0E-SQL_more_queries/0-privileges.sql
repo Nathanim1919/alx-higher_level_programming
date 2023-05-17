d5d1f0cc80621aeba41b195c884017ffe29bbe56
Nathanim1919 <nathanim2tadele@gmail.com>
2023-05-17 13:44:27 -0700

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 --  a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANT FOR user_0d_1@localhost;
-SHOW GRANT FOR user_0d_2@localhost;
+SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANT FOR 'user_0d_2'@'localhost';
