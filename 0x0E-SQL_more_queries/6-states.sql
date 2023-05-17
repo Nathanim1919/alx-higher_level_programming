edf33c49d2a086d3d900ce06d9df007cea06f340
Nathanim1919 <nathanim2tadele@gmail.com>
2023-05-17 15:02:51 -0700

    States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- a script that creates the database hbtn_0d_usa
 -- table states (in the database hbtn_0d_usa) 
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
 CREATE TABLE IF NOT EXISTS states(id INT UNIQUE AUTO_INCREMENT NOT NULL, PRIMARY KEY (id), name VARCHAR(256));
