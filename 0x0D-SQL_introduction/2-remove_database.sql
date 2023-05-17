1a0b615502311608fa6a45579656d9a3fd9e82bb
Nathanim1919 <nathanim2tadele@gmail.com>
2023-05-17 03:31:58 -0700

    delete database

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- Script that deletes the databse hbtn_0c_0
 -- if the databse does not  exist the script should not fail
-DELETE DATABASE IF NOT EXISTS hbtn_0c_0;
+DROP DATABASE IF NOT EXISTS hbtn_0c_0;
