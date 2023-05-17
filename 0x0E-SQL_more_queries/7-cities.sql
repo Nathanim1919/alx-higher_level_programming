39b3c67992ccc76d211b3b9012e8039b6340d851
Nathanim1919 <nathanim2tadele@gmail.com>
2023-05-17 15:24:30 -0700

    Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'cities'(
 	'id' INT UNIQUE AUTO_INCREMENT NOT NULL,
 	PRIMARY KEY (id),
 	'state_id' INT NOT NULL,
-        FOREIGN KEY (state_id) REFERENCES states(id)	
+        FOREIGN KEY (state_id) REFERENCES states(id),
 	'name' VARCHAR(256)
 );
