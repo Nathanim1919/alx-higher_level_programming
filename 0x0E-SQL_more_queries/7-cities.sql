e6dc841beb581263a4c716f8e7e0560002bec3c7
Nathanim1919 <nathanim2tadele@gmail.com>
2023-05-17 15:31:19 -0700

    Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 --  a script that creates the database hbtn_0d_usa
 --  create table cities
 
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	'id' INT UNIQUE AUTO_INCREMENT NOT NULL,
 	PRIMARY KEY (id),
