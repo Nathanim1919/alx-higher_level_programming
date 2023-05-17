daf3909fed03208af49c564d64faa73eefed7760
Nathanim1919 <nathanim2tadele@gmail.com>
2023-05-17 05:01:48 -0700

    Number by score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+--  a script that lists the number of records with the same score in the table second_table
+SELECT score, COUNT('score') as number
+FROM second_table
+GROUP BY score
+ORDER BY score DESC;
