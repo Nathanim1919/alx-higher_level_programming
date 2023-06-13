a1372e0c3a9fd86df350b8cc9feb9802c7d7d747
Nathanim1919 <nathanim2tadele@gmail.com>
2023-06-13 06:00:13 +0000

    an integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const number = parseInt(process.argv[2])
 
-if (isNaN(number) || process.argv[2] === undefined) {
+if (isNaN(number)) {
   console.log('Not a number')
 } else {
   console.log('My number: ', number)
