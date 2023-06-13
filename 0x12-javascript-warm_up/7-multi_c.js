f064b8992a63df8c1426f33b3d3f9d090842f2da
Nathanim1919 <nathanim2tadele@gmail.com>
2023-06-13 06:40:49 +0000

    i love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < Number(process.argv[2]); i++) {
-    console.log('C is fun')
+    console.log('C is fun');
   }
 }
