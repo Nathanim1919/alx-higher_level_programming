eeed275f4fe91f925d80f6461aab477321eed706
Nathanim1919 <nathanim2tadele@gmail.com>
2023-06-14 07:08:37 +0000

    Occurence

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,9 @@
 exports.nbOccurences = function (list, searchElement) {
   let occurance = 0;
   for (let i = 0; i < list.length; i++) {
-    if (searchElement === list[i]) occurance++;
-  }
-return occurance;
+    if (searchElement === list[i]) {
+        occurance++;
+     }
+   }
+  return occurance;
 }
