b0762498ab896b5fea45a3640a0f6ebcd9a555d7
Nathanim1919 <nathanim2tadele@gmail.com>
2023-06-17 10:47:13 -0700

    sql injection..

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,4 +1,4 @@
-#!?usr/bin/python3
+#!/usr/bin/python3
 """ a script that takes in arguments and displays all values in the states table
     of hbtn_0e_0_usa where name matches the argument.
      script should take 4 arguments: mysql username, mysql password, database name
