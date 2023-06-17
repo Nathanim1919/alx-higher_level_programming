f44b7e281383d90d3091fd0ad7b2419f1fd4870a
Nathanim1919 <nathanim2tadele@gmail.com>
2023-06-17 13:50:41 -0700

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
-""" a script that takes in the name of a state as an argument and
-lists all cities of that state, using the database hbtn_0e_4_usa
-"""
+"""takes in the name of a state as an argument and
+lists all cities of that state,
+using the database hbtn_0e_4_usa"""
 
 if __name__ == '__main__':
 
-    import sys
     import MySQLdb
+    import sys
 
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
@@ -21,4 +20,3 @@ if __name__ == '__main__':
     rows = cur.fetchall()
     print(", ".join([row[0] for row in rows]))
     cur.close()
-    db.close(
