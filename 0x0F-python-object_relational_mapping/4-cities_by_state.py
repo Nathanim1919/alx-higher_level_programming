46ce00c89b966542257452a98d4420c0abb82258
Nathanim1919 <nathanim2tadele@gmail.com>
2023-06-17 13:27:45 -0700

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,36 +1,19 @@
 #!/usr/bin/python3
-"""
-takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa
-where name matches the argument
+"""lists all cities from the database hbtn_0e_4_usa"""
 
-"""
 if __name__ == '__main__':
-    import sys
+
     import MySQLdb
+    import sys
 
-    try:
-        db = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=sys.argv[1],
-                passwd=sys.argv[2],
-                db=sys.argv[3]
-            )
-        cursor = db.cursor()
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-        cursor.execute("SELECT cities.id, cities.name, states.name\
-                        FROM cities LEFT JOIN states\
-                        ON states.id = cities.sate_id\
-                        ORDER BY cities.id ASC")
-
-        result = cursor.fetchall()
-        for row in result:
-            print(row)
-
-    except Exception:
-        print("Failed to connect with databse")
-        exit(0)
-
-    cursor.close()
-    db.close()
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities LEFT JOIN states\
+                ON states.id = cities.state_id\
+                ORDER BY cities.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
