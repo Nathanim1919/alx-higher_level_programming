ccbd19a1428736e373d9b1a1c8486008c286df2f
Nathanim1919 <nathanim2tadele@gmail.com>
2023-06-17 09:19:29 -0700

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
 
     try:
         db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
+                host="localhost",
+                port=3306,
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3]
             )
         cursor = db.cursor()
 
