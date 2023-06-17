c55a3649f65fd19c117e771521d44107b3c80191
Nathanim1919 <nathanim2tadele@gmail.com>
2023-06-17 10:35:29 -0700

    sql injection..

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
                 )
         cursor = db.cursor()
 
-        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(sys.argv[4]))
+        cursor.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id ASC", (sys.argv[4],))
 
         result = cursor.fetchall()
         for row in result:
