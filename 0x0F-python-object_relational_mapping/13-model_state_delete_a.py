d602d07b54ff76bc7fe42af1286827c97ca6cf57
Nathanim1919 <nathanim2tadele@gmail.com>
2023-06-18 09:22:18 -0700

    delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa """
+""" deletes all State objects with a name containing the letter a from
+the database hbtn_0e_6_usa
+"""
 
 if __name__ == "__main__":
 
