eb78432d86781925c513e39e8f0164e222e2c620
Nathanim1919 <nathanim2tadele@gmail.com>
2023-06-18 06:46:03 -0700

    Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -15,8 +15,7 @@ if __name__ == '__main__':
 
     session = Session(True)
 
-    state = session.query(State).filter(State.id == 2)
-
+    state = session.query(State).filter(State.id == 2).first()
     state.name = 'New Mexico'
     session.commit()
     session.close()
