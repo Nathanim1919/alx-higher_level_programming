9982608d9b65471c0e6cd035ff67a2c627a32f2a
Nathanim1919 <nathanim2tadele@gmail.com>
2023-06-18 06:01:16 -0700

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     import sys
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
-    from model_state from Base, State
+    from model_state import Base, State
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
             .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
