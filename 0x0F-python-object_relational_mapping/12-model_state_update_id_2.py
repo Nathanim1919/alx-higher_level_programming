c3e99ea4f3e32656820c6844fc01a9c8e0f0668e
Nathanim1919 <nathanim2tadele@gmail.com>
2023-06-18 06:39:17 -0700

    Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     from model_state import Base, State
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_pint=True)
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
 
