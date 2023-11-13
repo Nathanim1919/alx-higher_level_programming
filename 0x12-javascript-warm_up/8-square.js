257a0a60da30184731c1d71a673a368fa41f4eca
Nathanim1919 <nathanim2tadele@gmail.com>
2023-11-13 15:02:49 -0800

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const size = process.argv[2];
+const num = parseInt(size);
+const character = 'X';
+
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log(character.repeat(i));
+  }
+}
