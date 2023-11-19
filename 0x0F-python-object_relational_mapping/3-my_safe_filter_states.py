671d3b9985a9d7763ff4244d5fe7f4c003b19813
Nathanim1919 <nathanim2tadele@gmail.com>
2023-11-19 11:57:54 -0800

    safe filter

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     import sys
 
     db = MySQLdb.connect(host='localhost', port=3306,
-            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
     match = sys.argv[4]
