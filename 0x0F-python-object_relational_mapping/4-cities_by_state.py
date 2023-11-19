f11d71120f50b054edbd54d8f7766787f0d01169
Nathanim1919 <nathanim2tadele@gmail.com>
2023-11-19 13:09:24 -0800

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute('SELECT cities.id, cities.name, states.name\
-                FROM cities LEFT JOIN staes\
+                FROM cities LEFT JOIN states\
                 ON states.id = cities.state_id\
                 ORDER BY cities.id ASC')
 
