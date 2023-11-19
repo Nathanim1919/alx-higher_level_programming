ab1a6561c232503bf774b8f2cf32df15b81ca388
Nathanim1919 <nathanim2tadele@gmail.com>
2023-11-19 09:52:52 -0800

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""displays all values in the states table of 
+"""takes in an argument and displays all 
+values in the states table of 
 hbtn_0e_0_usa where name matches the argument"""
 
 
