faadbefaab4e97fe579bcfc155a4ceaa05d08273
Nathanim1919 <nathanim2tadele@gmail.com>
2023-12-04 15:34:16 -0800

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use curl to fetch the URL and display the size of the body in bytes
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep "Content-Length" | awk '{print $2}' | tr -d '\r'
