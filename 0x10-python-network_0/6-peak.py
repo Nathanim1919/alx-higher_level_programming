f68d60970881fdb38760d286d1f3771e64f34e2b
Nathanim1919 <nathanim2tadele@gmail.com>
2023-12-05 12:58:32 -0800

    find apeak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,14 +10,15 @@ Description: Provide a `find_peak` function
 def find_peak(list_of_integers):
     """Find the largest number in a list of unsorted integers
     """
-    peak = 0
-    last = len(list_of_integers) - 1
+    if (list_of_integers):
+        peak = 0
+        last = len(list_of_integers) - 1
 
-    while (peak < last):
-        mid = (peak + last) // 2
+        while (peak < last):
+            mid = (peak + last) // 2
 
-        if (list_of_integers[mid] < list_of_integers[mid + 1]):
-            peak = mid + 1
-        else:
-            last = mid
-    return list_of_integers[peak]
+            if (list_of_integers[mid] < list_of_integers[mid + 1]):
+                peak = mid + 1
+            else:
+                last = mid
+        return list_of_integers[peak]
