e2f51f9daf4e2b15057e6628cb6c2eaf3289d80e
Nathanim1919 <nathanim2tadele@gmail.com>
2023-12-05 12:52:19 -0800

    find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,21 @@
-# a function that finds  apeak in alist of unsorted integers
+#!/usr/bin/python3
+
+
+"""
+Module Name: 6-peak.py
+Description: Provide a `find_peak` function
+"""
+
 
 def find_peak(list_of_integers):
-    list_length = len(list_of_integers)
+    peak = 0
+    last = len(list_of_integers) - 1
 
-    for (i in range(1, list_lenght - 1)):
-        if (list_of_integers[i - 1] <= list_of_integers[i] >= list_of_integers[i + 1]):
-            return list_of_integers[i]
+    while (peak < last):
+        mid = (peak + last) // 2
+
+        if (list_of_integers[mid] < list_of_integers[mid + 1]):
+            peak = mid + 1
+        else:
+            last = mid
+    return list_of_integers[peak]
