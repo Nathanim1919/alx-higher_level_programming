167573e651bd8b61f1a373dc6217ff4e345024d1
Nathanim1919 <nathanim2tadele@gmail.com>
2023-12-05 17:25:37 -0800

    What's my status?#0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
+if __name__ == "__main__":
+
+    import urllib.request
+
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+
+    print("Body response:")
+    print("     - type:", type(content))
+    print("     - content:", content)
+    print("     - utf8 content:", content.decode('utf-8'))
