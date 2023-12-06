906ef21716685a27511ee25aed21592a4ef56745
Nathanim1919 <nathanim2tadele@gmail.com>
2023-12-05 18:54:27 -0800

    print error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,6 +12,9 @@ from urllib.error import HTTPError
 if __name__ == '__main__':
     req = Request(sys.argv[1])
     try:
-        urlopen(req)
+        with urlopen(req) as response:
+            body = response.read().decode('utf-8')
     except HTTPError as e:
         print('Error code: ', e.code)
+    else:
+        print(body)
