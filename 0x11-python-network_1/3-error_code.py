ac5b85ec3abc42e9e03fdd5fad520eb655d400a2
Nathanim1919 <nathanim2tadele@gmail.com>
2023-12-05 18:50:43 -0800

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module Name: 3-error_code.py
+Description: a Python script that takes in a URL, sends a request to
+            `the URL and displays the body of the response (decoded in utf-8).
+"""
+
+import sys
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+
+if __name__ == '__main__':
+    req = Request(sys.argv[1])
+    try:
+        urlopen(req)
+    except HTTPError as e:
+        print('Error code: ', e.code)
