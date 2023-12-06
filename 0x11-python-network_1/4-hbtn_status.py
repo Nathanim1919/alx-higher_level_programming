c7b7df3ce44cb9726a03ccfc9d562160f79baeb2
Nathanim1919 <nathanim2tadele@gmail.com>
2023-12-06 10:25:21 -0800

    What's my status?

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module Name: 4-hbtn_status.py
+Description: Write a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+import requests
+
+
+if __name__ == '__main__':
+    res = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type:", type(res.text))
+    print("\t- content:", res.text)
