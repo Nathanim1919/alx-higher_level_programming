33f4916305c0f717a4f9e4babc94efb9e8db1098
Nathanim1919 <nathanim2tadele@gmail.com>
2023-12-05 18:02:47 -0800

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-
-"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
-
+"""
+Module Name: 0-hbtn_status.py
+Description: A script that fetches `https://alx-intranet.hbtn.io/status`
+"""
 from urllib.request import Request, urlopen
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     req = Request('https://alx-intranet.hbtn.io/status')
     with urlopen(req) as response:
-        content = response.read()
-
-    print("Body response:")
-    print("     - type:", type(content))
-    print("     - content:", content)
-    print("     - utf8 content:", content.decode('utf-8'))
+        res = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(res)}")
+        print(f"\t- content: {res}")
+        content = res.decode('utf-8')
+        print(f"\t- utf8 content: {content}")
