5dd2359d119da6ce5f22daf0e1e07f77152670d8
Nathanim1919 <nathanim2tadele@gmail.com>
2023-12-05 17:51:37 -0800

    0. What's my status?#0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 """a Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+
 if __name__ == "__main__":
-
-    import urllib.request
-
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         content = response.read()
