93c21c0564821ac51c57b68628e5979f9e874a49
root <nathanim2tadele@gmail.com>
2024-02-03 11:53:54 -0800

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,5 @@ const request = require("request");
 const url = process.argv[2];
 
 request.get(url, (error, response, body) => {
-  if (error) {
-   console.log(error);
-  } else {
    console.log("code: ", response.statusCode);
-  }
 });
